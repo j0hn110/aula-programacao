15942961130fe214c89e437df54fd6bef06162f6
j0hn110 <jgtescaro@gmail.com>
2026-02-19 08:29:28 -0300

    Create entrada.py

---
+++ entrada.py
@@ -0,0 +1,10 @@
+usuario = input('Digite seu nome de usuário: ')
+senha = input('Digite sua senha: ')
+dominio = input('Qual seria o domínio? ')
+print('Olá ', nome, ', seja bem vindo ao meu mundo')
+print('Sua senha é: ', senha)
+print('O seu domínio é: ', dominio)
+
+
+email = usuario +"@" + dominio
+print('Seu e-mail é: ', email)
